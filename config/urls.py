dc60c907d911d2ca51f7599ddf0185a81f5a69b2
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-09-01 17:25:42 +0300

    complete

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('pages.urls')),
+    path('snacks/',include('snacks.url'))
 ]
 
 if settings.DEBUG:
